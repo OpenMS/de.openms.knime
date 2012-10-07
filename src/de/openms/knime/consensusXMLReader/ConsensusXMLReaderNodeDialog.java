54ac7cec2fd00662dd2f9e887d760de226f858ca
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-10-07 22:34:51 +0200

    added jaxb based cXML reader
    will switch to SAX in near future

--- src/de/openms/knime/consensusXMLReader/ConsensusXMLReaderNodeDialog.java
+++ src/de/openms/knime/consensusXMLReader/ConsensusXMLReaderNodeDialog.java
@@ -1,37 +1,25 @@
 package de.openms.knime.consensusXMLReader;
 
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
-import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
-import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 
 /**
- * <code>NodeDialog</code> for the "ConsensusXMLReader" Node.
- * Converts consesnsusXML files to KNIME tables.	
- *
+ * <code>NodeDialog</code> for the "ConsensusXMLReader" Node. Converts
+ * consesnsusXML files to KNIME tables.
+ * 
  * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
- * creation of a simple dialog with standard components. If you need a more 
- * complex dialog please derive directly from 
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
  * {@link org.knime.core.node.NodeDialogPane}.
  * 
  * @author The OpenMS Team
  */
 public class ConsensusXMLReaderNodeDialog extends DefaultNodeSettingsPane {
 
-    /**
-     * New pane for configuring ConsensusXMLReader node dialog.
-     * This is just a suggestion to demonstrate possible default dialog
-     * components.
-     */
-    protected ConsensusXMLReaderNodeDialog() {
-        super();
-        
-        addDialogComponent(new DialogComponentNumber(
-                new SettingsModelIntegerBounded(
-                    ConsensusXMLReaderNodeModel.CFGKEY_COUNT,
-                    ConsensusXMLReaderNodeModel.DEFAULT_COUNT,
-                    Integer.MIN_VALUE, Integer.MAX_VALUE),
-                    "Counter:", /*step*/ 1, /*componentwidth*/ 5));
-                    
-    }
+	/**
+	 * New pane for configuring ConsensusXMLReader node dialog. This is just a
+	 * suggestion to demonstrate possible default dialog components.
+	 */
+	protected ConsensusXMLReaderNodeDialog() {
+		super();
+	}
 }
-
--- src/de/openms/knime/consensusXMLReader/ConsensusXMLReaderNodeModel.java
+++ src/de/openms/knime/consensusXMLReader/ConsensusXMLReaderNodeModel.java
@@ -2,21 +2,27 @@ package de.openms.knime.consensusXMLReader;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
-import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
-import org.knime.core.data.RowKey;
 import org.knime.core.data.def.DefaultRow;
 import org.knime.core.data.def.DoubleCell;
 import org.knime.core.data.def.IntCell;
 import org.knime.core.data.def.StringCell;
+import org.knime.core.data.url.MIMEType;
+import org.knime.core.data.url.URIContent;
+import org.knime.core.data.url.port.MIMEURIPortObject;
+import org.knime.core.data.url.port.MIMEURIPortObjectSpec;
 import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
-import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
@@ -24,197 +30,208 @@ import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
 
+import de.openms.consensusXML.ConsensusElement;
+import de.openms.consensusXML.ConsensusXML;
+import de.openms.consensusXML.Element;
 
 /**
- * This is the model implementation of ConsensusXMLReader.
- * Converts consesnsusXML files to KNIME tables.	
- *
+ * This is the model implementation of ConsensusXMLReader. Converts
+ * consesnsusXML files to KNIME tables.
+ * 
  * @author The OpenMS Team
  */
 public class ConsensusXMLReaderNodeModel extends NodeModel {
-    
-    // the logger instance
-    private static final NodeLogger logger = NodeLogger
-            .getLogger(ConsensusXMLReaderNodeModel.class);
-        
-    /** the settings key which is used to retrieve and 
-        store the settings (from the dialog or from a settings file)    
-       (package visibility to be usable from the dialog). */
-	static final String CFGKEY_COUNT = "Count";
 
-    /** initial default count value. */
-    static final int DEFAULT_COUNT = 100;
+	// the logger instance
+	private static final NodeLogger logger = NodeLogger
+			.getLogger(ConsensusXMLReaderNodeModel.class);
 
-    // example value: the models count variable filled from the dialog 
-    // and used in the models execution method. The default components of the
-    // dialog work with "SettingsModels".
-    private final SettingsModelIntegerBounded m_count =
-        new SettingsModelIntegerBounded(ConsensusXMLReaderNodeModel.CFGKEY_COUNT,
-                    ConsensusXMLReaderNodeModel.DEFAULT_COUNT,
-                    Integer.MIN_VALUE, Integer.MAX_VALUE);
-    
+	/**
+	 * Constructor for the node model.
+	 */
+	protected ConsensusXMLReaderNodeModel() {
+		super(new PortType[] { new PortType(MIMEURIPortObject.class) },
+				new PortType[] { new PortType(BufferedDataTable.class) });
+	}
 
-    /**
-     * Constructor for the node model.
-     */
-    protected ConsensusXMLReaderNodeModel() {
-    
-        // TODO one incoming port and one outgoing port is assumed
-        super(1, 1);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected BufferedDataTable[] execute(final PortObject[] inObjects,
+			final ExecutionContext exec) throws Exception {
+		logger.info("Node Model Stub... this is not yet implemented !");
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
-            final ExecutionContext exec) throws Exception {
+		MIMEURIPortObject obj = (MIMEURIPortObject) inObjects[0];
+		List<URIContent> uris = obj.getURIContents();
+		if (uris.size() == 0) {
+			throw new Exception(
+					"No URI was supplied in MIMEURIPortObject at input port 0");
+		} else if (uris.size() != 1) {
+			throw new Exception(String.format(
+					"We can only demangle a single file but got %d.",
+					uris.size()));
+		}
 
-        // TODO do something here
-        logger.info("Node Model Stub... this is not yet implemented !");
+		URI relURI = uris.get(0).getURI();
+		File cXMLFile = new File(relURI);
 
-        
-        // the data table spec of the single output table, 
-        // the table will have three columns:
-        DataColumnSpec[] allColSpecs = new DataColumnSpec[3];
-        allColSpecs[0] = 
-            new DataColumnSpecCreator("Column 0", StringCell.TYPE).createSpec();
-        allColSpecs[1] = 
-            new DataColumnSpecCreator("Column 1", DoubleCell.TYPE).createSpec();
-        allColSpecs[2] = 
-            new DataColumnSpecCreator("Column 2", IntCell.TYPE).createSpec();
-        DataTableSpec outputSpec = new DataTableSpec(allColSpecs);
-        // the execution context will provide us with storage capacity, in this
-        // case a data container to which we will add rows sequentially
-        // Note, this container can also handle arbitrary big data tables, it
-        // will buffer to disc if necessary.
-        BufferedDataContainer container = exec.createDataContainer(outputSpec);
-        // let's add m_count rows to it
-        for (int i = 0; i < m_count.getIntValue(); i++) {
-            RowKey key = new RowKey("Row " + i);
-            // the cells of the current row, the types of the cells must match
-            // the column spec (see above)
-            DataCell[] cells = new DataCell[3];
-            cells[0] = new StringCell("String_" + i); 
-            cells[1] = new DoubleCell(0.5 * i); 
-            cells[2] = new IntCell(i);
-            DataRow row = new DefaultRow(key, cells);
-            container.addRowToTable(row);
-            
-            // check if the execution monitor was canceled
-            exec.checkCanceled();
-            exec.setProgress(i / (double)m_count.getIntValue(), 
-                "Adding row " + i);
-        }
-        // once we are done, we close the container and return its table
-        container.close();
-        BufferedDataTable out = container.getTable();
-        return new BufferedDataTable[]{out};
-    }
+		JAXBContext jc = JAXBContext.newInstance("de.openms.consensusXML");
+		Unmarshaller u = jc.createUnmarshaller();
+		ConsensusXML cXMLDocument = (ConsensusXML) u.unmarshal(cXMLFile);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void reset() {
-        // TODO Code executed on reset.
-        // Models build during execute are cleared here.
-        // Also data handled in load/saveInternals will be erased here.
-    }
+		// create the container based on the data set
+		BufferedDataContainer container = exec
+				.createDataContainer(getFeatureTableSpec(cXMLDocument));
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
-            throws InvalidSettingsException {
-        
-        // TODO: check if user settings are available, fit to the incoming
-        // table structure, and the incoming types are feasible for the node
-        // to execute. If the node can execute in its current state return
-        // the spec of its output data table(s) (if you can, otherwise an array
-        // with null elements), or throw an exception with a useful user message
+		int mapCount = cXMLDocument.getMapList().getMap().size();
+		int colCount = 6 + (mapCount * 3);
+		int rowIdx = 1;
 
-        return new DataTableSpec[]{null};
-    }
+		for (ConsensusElement cElem : cXMLDocument.getConsensusElementList()
+				.getConsensusElement()) {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void saveSettingsTo(final NodeSettingsWO settings) {
+			DataCell[] rowCells = new DataCell[colCount];
+			rowCells[0] = new StringCell(cElem.getId());
+			rowCells[1] = new IntCell((cElem.getCharge() != null ? cElem
+					.getCharge().intValue() : -1));
+			rowCells[2] = new DoubleCell(cElem.getQuality());
+			rowCells[3] = new DoubleCell(cElem.getCentroid().getRt());
+			rowCells[4] = new DoubleCell(cElem.getCentroid().getMz());
+			rowCells[5] = new DoubleCell(cElem.getCentroid().getIt());
 
-        // TODO save user settings to the config object.
-        
-        m_count.saveSettingsTo(settings);
+			// fill cells
+			for (int i = 0; i < cXMLDocument.getMapList().getMap().size(); ++i) {
+				rowCells[(6 + (i * 3))] = new DoubleCell(-1);
+				rowCells[(7 + (i * 3))] = new DoubleCell(-1);
+				rowCells[(8 + (i * 3))] = new DoubleCell(-1);
+			}
 
-    }
+			// add real values
+			for (Element elem : cElem.getGroupedElementList().getElement()) {
+				rowCells[(int) (6 + (elem.getMap() * 3))] = new DoubleCell(
+						elem.getRt());
+				rowCells[(int) (7 + (elem.getMap() * 3))] = new DoubleCell(
+						elem.getMz());
+				rowCells[(int) (8 + (elem.getMap() * 3))] = new DoubleCell(
+						elem.getIt());
+			}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
-            throws InvalidSettingsException {
-            
-        // TODO load (valid) settings from the config object.
-        // It can be safely assumed that the settings are valided by the 
-        // method below.
-        
-        m_count.loadSettingsFrom(settings);
+			container.addRowToTable(new DefaultRow(String.format("Row %d",
+					rowIdx++), rowCells));
+		}
 
-    }
+		container.close();
+		BufferedDataTable out = container.getTable();
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void validateSettings(final NodeSettingsRO settings)
-            throws InvalidSettingsException {
-            
-        // TODO check if the settings could be applied to our model
-        // e.g. if the count is in a certain range (which is ensured by the
-        // SettingsModel).
-        // Do not actually set any values of any member variables.
+		return new BufferedDataTable[] { out };
+	}
 
-        m_count.validateSettings(settings);
+	private DataTableSpec getFeatureTableSpec(ConsensusXML cXMLDocument) {
 
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void loadInternals(final File internDir,
-            final ExecutionMonitor exec) throws IOException,
-            CanceledExecutionException {
-        
-        // TODO load internal data. 
-        // Everything handed to output ports is loaded automatically (data
-        // returned by the execute method, models loaded in loadModelContent,
-        // and user settings set through loadSettingsFrom - is all taken care 
-        // of). Load here only the other internals that need to be restored
-        // (e.g. data used by the views).
+		DataColumnSpec[] allColSpecs = new DataColumnSpec[6 + (cXMLDocument
+				.getMapList().getMap().size() * 3)];
 
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void saveInternals(final File internDir,
-            final ExecutionMonitor exec) throws IOException,
-            CanceledExecutionException {
-       
-        // TODO save internal models. 
-        // Everything written to output ports is saved automatically (data
-        // returned by the execute method, models saved in the saveModelContent,
-        // and user settings saved through saveSettingsTo - is all taken care 
-        // of). Save here only the other internals that need to be preserved
-        // (e.g. data used by the views).
+		// the general values
+		allColSpecs[0] = new DataColumnSpecCreator("ID", StringCell.TYPE)
+				.createSpec();
+		allColSpecs[1] = new DataColumnSpecCreator("charge", IntCell.TYPE)
+				.createSpec();
+		allColSpecs[2] = new DataColumnSpecCreator("quality", DoubleCell.TYPE)
+				.createSpec();
+		allColSpecs[3] = new DataColumnSpecCreator("centroid_rt",
+				DoubleCell.TYPE).createSpec();
+		allColSpecs[4] = new DataColumnSpecCreator("centroid_mz",
+				DoubleCell.TYPE).createSpec();
+		allColSpecs[5] = new DataColumnSpecCreator("centroid_intensity",
+				DoubleCell.TYPE).createSpec();
 
-    }
+		for (int i = 0; i < cXMLDocument.getMapList().getMap().size(); ++i) {
+			allColSpecs[6 + (i * 3)] = new DataColumnSpecCreator(String.format(
+					"map_%d_rt", i), DoubleCell.TYPE).createSpec();
+			allColSpecs[7 + (i * 3)] = new DataColumnSpecCreator(String.format(
+					"map_%d_mz", i), DoubleCell.TYPE).createSpec();
+			allColSpecs[8 + (i * 3)] = new DataColumnSpecCreator(String.format(
+					"map_%d_intensity", i), DoubleCell.TYPE).createSpec();
+		}
+
+		return new DataTableSpec(allColSpecs);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void reset() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected DataTableSpec[] configure(final PortObjectSpec[] inSpecs)
+			throws InvalidSettingsException {
+		// we do not know how the table looks before we read the complete
+		// consensusXML file
+
+		if (!(inSpecs[0] instanceof MIMEURIPortObjectSpec)) {
+			throw new InvalidSettingsException(
+					"no MIMEURIPortObject compatible port object at port 0");
+		}
+
+		MIMEURIPortObjectSpec spec = (MIMEURIPortObjectSpec) inSpecs[0];
+		if (!spec.getMIMEType().equals(new MIMEType("consensusxml"))) {
+			throw new InvalidSettingsException(
+					"This node can only parse consensusXML files but got "
+							+ spec.getMIMEType() + ".");
+		}
+
+		return new DataTableSpec[] { null };
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveSettingsTo(final NodeSettingsWO settings) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void validateSettings(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+	}
 
 }
-
