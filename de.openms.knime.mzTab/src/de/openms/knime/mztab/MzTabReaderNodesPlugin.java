17f89872d0753e07f35e502f83dcb2ed7c60368c
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-07-08 21:16:59 +0200

    added/updated copyright header to source files

--- de.openms.knime.mzTab/src/de/openms/knime/mztab/MzTabReaderNodesPlugin.java
+++ de.openms.knime.mzTab/src/de/openms/knime/mztab/MzTabReaderNodesPlugin.java
@@ -1,6 +1,32 @@
-/* @(#)$RCSfile$ 
- * $Revision$ $Date$ $Author$
- *
+/**
+ * --------------------------------------------------------------------------
+ *                   OpenMS -- Open-Source Mass Spectrometry
+ * --------------------------------------------------------------------------
+ * Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+ * ETH Zurich, and Freie Universitaet Berlin 2002-2013.
+ * 
+ * This software is released under a three-clause BSD license:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of any author or any participating institution
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * For a full list of authors, refer to the file AUTHORS.
+ * --------------------------------------------------------------------------
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+ * INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package de.openms.knime.mztab;
 
@@ -8,58 +34,60 @@ import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * This is the eclipse bundle activator.
- * Note: KNIME node developers probably won't have to do anything in here, 
- * as this class is only needed by the eclipse platform/plugin mechanism.
- * If you want to move/rename this file, make sure to change the plugin.xml
- * file in the project root directory accordingly.
- *
+ * This is the eclipse bundle activator. Note: KNIME node developers probably
+ * won't have to do anything in here, as this class is only needed by the
+ * eclipse platform/plugin mechanism. If you want to move/rename this file, make
+ * sure to change the plugin.xml file in the project root directory accordingly.
+ * 
  * @author Stephan Aiche on behalf of the OpenMS Team
  */
 public class MzTabReaderNodesPlugin extends Plugin {
-    // The shared instance.
-    private static MzTabReaderNodesPlugin plugin;
+	// The shared instance.
+	private static MzTabReaderNodesPlugin plugin;
 
-    /**
-     * The constructor.
-     */
-    public MzTabReaderNodesPlugin() {
-        super();
-        plugin = this;
-    }
+	/**
+	 * The constructor.
+	 */
+	public MzTabReaderNodesPlugin() {
+		super();
+		plugin = this;
+	}
 
-    /**
-     * This method is called upon plug-in activation.
-     * 
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be started
-     */
-    @Override
-    public void start(final BundleContext context) throws Exception {
-        super.start(context);
+	/**
+	 * This method is called upon plug-in activation.
+	 * 
+	 * @param context
+	 *            The OSGI bundle context
+	 * @throws Exception
+	 *             If this plugin could not be started
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
 
-    }
+	}
 
-    /**
-     * This method is called when the plug-in is stopped.
-     * 
-     * @param context The OSGI bundle context
-     * @throws Exception If this plugin could not be stopped
-     */
-    @Override
-    public void stop(final BundleContext context) throws Exception {
-        super.stop(context);
-        plugin = null;
-    }
+	/**
+	 * This method is called when the plug-in is stopped.
+	 * 
+	 * @param context
+	 *            The OSGI bundle context
+	 * @throws Exception
+	 *             If this plugin could not be stopped
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
 
-    /**
-     * Returns the shared instance.
-     * 
-     * @return Singleton instance of the Plugin
-     */
-    public static MzTabReaderNodesPlugin getDefault() {
-        return plugin;
-    }
+	/**
+	 * Returns the shared instance.
+	 * 
+	 * @return Singleton instance of the Plugin
+	 */
+	public static MzTabReaderNodesPlugin getDefault() {
+		return plugin;
+	}
 
 }
-
--- de.openms.knime.startupCheck/src/de/openms/knime/startupcheck/Activator.java
+++ de.openms.knime.startupCheck/src/de/openms/knime/startupcheck/Activator.java
@@ -3,7 +3,7 @@
  *                   OpenMS -- Open-Source Mass Spectrometry
  * --------------------------------------------------------------------------
  * Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
- * ETH Zurich, and Freie Universitaet Berlin 2002-2012.
+ * ETH Zurich, and Freie Universitaet Berlin 2002-2013.
  * 
  * This software is released under a three-clause BSD license:
  *  * Redistributions of source code must retain the above copyright
@@ -53,6 +53,7 @@ public class Activator extends AbstractUIPlugin {
 	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
 	 * )
 	 */
+	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		super.start(bundleContext);
 		INSTANCE = this;
@@ -64,6 +65,7 @@ public class Activator extends AbstractUIPlugin {
 	 * @see
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
+	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		super.stop(bundleContext);
 	}
